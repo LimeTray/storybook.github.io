0ab27af1d955ed48e25532fe9a8dce15ea515b6d
lt-login <lt.login@limetray.com>
2024-09-09 18:11:25 +0530

    Deployed from Jenkins: https://deployment.limetray.com/ | Build Number: 65

---
+++ react-storybook/assets/StepperFooter.stories-4c8a2748.js
@@ -0,0 +1,22 @@
+import{j as a}from"./jsx-runtime-8406ef1e.js";import{r as i}from"./index-ad58220e.js";import{S as u}from"./stepperFooter-0d08e14d.js";import"./_commonjsHelpers-de833af9.js";import"./ltButton-e78664a7.js";import"./Button-8f1e9999.js";import"./extends-4c19d496.js";import"./inheritsLoose-5494d9cc.js";import"./factories-03e2068e.js";import"./getElementType-486fb75c.js";import"./childrenUtils-8c0966fa.js";import"./Icon-a36dbc07.js";import"./Label-3b77b452.js";import"./ModernAutoControlledComponent-4ca57a24.js";import"./assertThisInitialized-081f9914.js";import"./htmlPropsUtils-cd6198e4.js";import"./includes-6af2fe54.js";import"./Portal-3e499843.js";import"./index-4fee7e45.js";import"./Ref-51c42093.js";import"./objectWithoutPropertiesLoose-f3b3ace0.js";import"./map-0dadb82a.js";const q={title:"StepperFooter",component:u,tags:["autodocs"]},C=e=>{const[S,o]=i.useState(1),[n,p]=i.useState(!1),d=()=>{o(r=>Math.max(0,r-1)),p(!1)},x=()=>{o(r=>{const s=Math.min(e.totalSteps,r+1);return s===e.totalSteps&&p(!0),s})};return a.jsx("div",{style:{width:"300px"},children:a.jsx(u,{...e,currentStep:S,onPrevClick:d,onNextClick:x,nextButtonLabel:n?"Done":"Next",isCompleted:n})})},t=C.bind({});t.args={totalSteps:3,previousButtonLabel:"Previous",nextButtonLabel:"Next",steptext:"Step"};var m,l,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const handlePrev = () => {
+    setCurrentStep(prevStep => Math.max(0, prevStep - 1));
+    setIsCompleted(false);
+  };
+  const handleNext = () => {
+    setCurrentStep(prevStep => {
+      const nextStep = Math.min(args.totalSteps, prevStep + 1);
+      if (nextStep === args.totalSteps) {
+        setIsCompleted(true);
+      }
+      return nextStep;
+    });
+  };
+  return <div style={{
+    width: "300px"
+  }}>
+      <StepperFooter {...args} currentStep={currentStep} onPrevClick={handlePrev} onNextClick={handleNext} nextButtonLabel={isCompleted ? "Done" : "Next"} isCompleted={isCompleted} />
+    </div>;
+}`,...(c=(l=t.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const z=["Default"];export{t as Default,z as __namedExportsOrder,q as default};
