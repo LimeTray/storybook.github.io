6c92e41147358307aedad8f0fc6fcf60e248f21c
lt-login <lt.login@limetray.com>
2025-01-06 14:39:23 +0530

    Deployed from Jenkins: https://deployment.limetray.com/ | Build Number: 108

---
+++ react-storybook/assets/LtModal.stories-671b2301.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-8406ef1e.js";import{r as a}from"./index-ad58220e.js";import{M as h}from"./ltModal-371a78c3.js";import{B as d}from"./Button-21321eef.js";import{I as f}from"./Icon-370c2d23.js";import{I as u}from"./Input-3f8aa787.js";import{L as y}from"./legends-66516f4a.js";import"./_commonjsHelpers-de833af9.js";import"./Modal-220c81f3.js";import"./extends-4c19d496.js";import"./inheritsLoose-5494d9cc.js";import"./factories-df82d7d3.js";import"./getElementType-ff0d42f5.js";import"./pick-729eb0ec.js";import"./_baseSet-d58de3a8.js";import"./childrenUtils-dd3b0ba5.js";import"./includes-192f9362.js";import"./index-f77928b3.js";import"./_arrayReduce-865b0d3f.js";import"./isEmpty-b690c58c.js";import"./ModernAutoControlledComponent-6bbe7d3d.js";import"./assertThisInitialized-081f9914.js";import"./Portal-8d2f321a.js";import"./index-4fee7e45.js";import"./Ref-51c42093.js";import"./objectWithoutPropertiesLoose-f3b3ace0.js";import"./map-20538cdf.js";import"./crossIcon-11d15636.js";import"./checkIcon-e2b8e3fe.js";import"./Label-d5345000.js";import"./htmlPropsUtils-2b22d933.js";const te={component:h,title:"LtModal",tags:["autodocs"]},V=[{text:"Superb!",description:"(<8 mins)",color:"#00FF00"},{text:"Acceptable",description:"(8 mins-11 mins)",color:"#FFFF00"},{text:"Alarming",description:"(>11 mins)",color:"#FF0000"}],j=({modalProps:x})=>{const[C,l]=a.useState(!1),[s,r]=a.useState(""),[p,i]=a.useState(""),v=()=>l(!0),t=()=>l(!1),I=()=>{console.log("Input 1:",s),console.log("Input 2:",p),r(""),i(""),t()},M="Legend";return e.jsxs("div",{children:[e.jsx("button",{onClick:v,children:"Open Modal"}),e.jsx(h,{...x,open:C,onClose:t,children:e.jsxs("div",{children:[e.jsx("h5",{children:"Enter Goal Range (in Minutes)"}),e.jsxs("div",{style:{display:"flex"},children:[e.jsx(u,{placeholder:"Min value",value:s,onChange:o=>r(o.target.value),style:{margin:"10px",padding:"0px"}}),e.jsx(u,{placeholder:"Min value",value:p,onChange:o=>i(o.target.value),style:{margin:"10px",padding:"0px"}})]}),e.jsx(y,{heading:M,legendsData:V}),e.jsxs("div",{style:{marginTop:"20px",display:"flex"},children:[e.jsx(d,{content:"Cancel",onClick:t}),e.jsx(d,{content:"Create",color:"green",onClick:I})]})]})})]})},n=j.bind({});n.args={modalProps:{header:"Goal Setup for {metric name}",showCloseIcon:!0,headerIcon:e.jsx(f,{name:"close"}),className:"my-custom-class"}};var c,m,g;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`({
+  modalProps
+}) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+  const handleCreate = () => {
+    console.log("Input 1:", inputValue1);
+    console.log("Input 2:", inputValue2);
+    setInputValue1("");
+    setInputValue2("");
+    handleClose();
+  };
+  const heading = "Legend";
+  return <div>
+      <button onClick={handleOpen}>Open Modal</button>
+      <MyModal {...modalProps} open={modalOpen} onClose={handleClose}>
+        <div>
+          <h5>Enter Goal Range (in Minutes)</h5>
+          <div style={{
+          display: "flex"
+        }}>
+            <Input placeholder="Min value" value={inputValue1} onChange={e => setInputValue1(e.target.value)} style={{
+            margin: "10px",
+            padding: "0px"
+          }} />
+            <Input placeholder="Min value" value={inputValue2} onChange={e => setInputValue2(e.target.value)} style={{
+            margin: "10px",
+            padding: "0px"
+          }} />
+          </div>
+          <Legends heading={heading} legendsData={legendsData} />
+          <div style={{
+          marginTop: "20px",
+          display: "flex"
+        }}>
+            <Button content="Cancel" onClick={handleClose} />
+            <Button content="Create" color="green" onClick={handleCreate} />
+          </div>
+        </div>
+      </MyModal>
+    </div>;
+}`,...(g=(m=n.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};const oe=["Default"];export{n as Default,oe as __namedExportsOrder,te as default};
